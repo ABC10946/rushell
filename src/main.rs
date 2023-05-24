322832da8da1212f4133997730820be54d0387eb
ABC10946 <abctech10946@gmail.com>
2023-05-24 23:41:00 +0900

    add feature loop

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,20 @@ use std::io::Write;
 use std::process::Command;
 
 fn main() {
-    let mut buffer = String::new();
-    print!("$ ");
-    io::stdout().flush().unwrap(); // for print prompt before input
-    io::stdin().read_line(&mut buffer).unwrap(); // input command
+    loop {
+        let mut buffer = String::new();
+        print!("$ ");
+        io::stdout().flush().unwrap(); // for print prompt before input
+        io::stdin().read_line(&mut buffer).unwrap(); // input command
 
-    let commands: Vec<&str> = buffer.trim_end().split(" ").collect();
+        let commands: Vec<&str> = buffer.trim_end().split(" ").collect();
 
-    let output = Command::new(commands[0])
-        .args(&commands[1..])
-        .output()
-        .expect("failed to execute command");
-    let output_str = String::from_utf8_lossy(&output.stdout);
+        let output = Command::new(commands[0])
+            .args(&commands[1..])
+            .output()
+            .expect("failed to execute command");
+        let output_str = String::from_utf8_lossy(&output.stdout);
 
-    println!("{}", output_str);
+        print!("{}", output_str);
+    }
 }
